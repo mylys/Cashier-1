9691aeb52a5bf8be1ea352ff6b84aa86cf1dfaea
SC-201805201737\Administrator <1159658171@qq.com>
2018-12-19 00:22:54 +0800

    历史订单退货页UI

--- app/src/main/java/com/easygo/cashier/module/order_history/OrderHistoryRefundFragment.java
+++ app/src/main/java/com/easygo/cashier/module/order_history/OrderHistoryRefundFragment.java
@@ -8,29 +8,37 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.easygo.cashier.R;
+import com.easygo.cashier.widget.MySearchView;
 import com.niubility.library.base.BaseFragment;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
 public class OrderHistoryRefundFragment extends BaseFragment {
 
+    @BindView(R.id.search_view)
+    MySearchView searchView;
     private Unbinder unbinder;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_order_history_refund, container, false);
-
         unbinder = ButterKnife.bind(this, view);
+
+        initView();
         return view;
+    }
+
+    private void initView() {
 
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        if(unbinder != null)
+        if (unbinder != null)
             unbinder.unbind();
     }
 
